7960152bc4f1a68ded6852512be6145bda87e50b
JulianMolina09 <soccerj703@gmail.com>
2023-01-27 08:19:24 -0500

    Canvas

--- Python/ios.py
+++ Python/ios.py
@@ -2,6 +2,7 @@ import requests
 import datetime
 import pytz
 import pandas as pd
+from flask import Flask, render_template
 
 # Create a session to handle the requests
 session = requests.Session()
@@ -21,41 +22,20 @@ def format_date(date_str):
     return date_time_obj_est.strftime('%Y-%m-%d %I:%M:%S %p')
 
 # Get the current date
+# Get the current date
 now = datetime.datetime.now()
 
 # Get the start and end of the current week
 week_start = now - datetime.timedelta(days=now.weekday())
 week_end = week_start + datetime.timedelta(days=6)
 
-# Iterate through each course id
-for course in aps_ids + nvcc_ids:
-    if course in aps_ids:
-        api_url = aps_url
-        key = aps_key
-    else:
-        api_url = nvcc_url
-        key = nvcc_key
-    # make API call to get the assignment
-    response = session.get(f"{api_url}/courses/{course}/assignments?per_page=100", headers={"Authorization": f"Bearer {key}"})
-    if response.status_code == 200:
-        # parse the response
-        assignments = response.json()
-        for assignment in assignments:
-            if assignment["due_at"]:
-                date_time_str = str(assignment['due_at'])
-                due_date = datetime.datetime.strptime(date_time_str, '%Y-%m-%dT%H:%M:%SZ')
-                # check if the due date is within the current week
-                if week_start <= due_date <= week_end:
-                    formatted_date = format_date(date_time_str)
-                    print(f"{assignment['name']}: {formatted_date}")
-                    
-                    # Create a dataframe to store the assignments
+# Create a dataframe to store the assignments
 # Try to read the existing excel sheet
 try:
     df = pd.read_excel("assignments.xlsx")
 except:
-    # If the sheet doesn't exist, create a new dataframe
-    df = pd.DataFrame(columns=['Name', 'Due Date', 'Course'])
+    # If the sheet doesn't exist, create a new dataframe with the status column
+    df = pd.DataFrame(columns=['Name', 'Due Date', 'Course', 'Status'])
 
 # Iterate through each course id
 for course in aps_ids + nvcc_ids:
@@ -77,9 +57,33 @@ for course in aps_ids + nvcc_ids:
                 # check if the due date is within the current week
                 if week_start <= due_date <= week_end:
                     formatted_date = format_date(date_time_str)
+                    status = ""
+                    if due_date < now:
+                        status = "late"
+                    else:
+                        status = "on time"
                     # Add the assignment to the dataframe
-                    df = df.append({'Name': assignment['name'], 'Due Date': formatted_date, 'Course': course}, ignore_index=True)
+                    if (course) == '479410':
+                        (course) = "English"
+                    elif (course) == '85762':
+                        (course) = "History"
+                    elif (course) == '85633':
+                        (course) = "Math"
+                    elif (course) == '85157':
+                        (course) = "Physics"
+                    df = df.append({'Name': assignment['name'], 'Due Date': formatted_date, 'Course': course, 'Status': status}, ignore_index=True)
+df = df.drop_duplicates()
+df = df.style.set_properties(**{'text-align': 'left', 'white-space': 'pre-wrap'})
 
 # Export the dataframe to an Excel sheet
 df.to_excel("assignments.xlsx", index=False)
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = pd.read_excel("assignments.xlsx")
+    return render_template("canvas.html", data=data.to_html())
+
+if __name__ == '__main__':
+    app.run(debug=True)
